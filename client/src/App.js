1951f76a6ea3d1b3abda1db7e1437bf350277179
Kaoutar Ahannuk <kaoutarahanuk@gmail.com>
2021-04-23 16:35:51 +0000

    sidebar
    "modifications"
    
    "SideBar"

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Login from './pages/login/Login';
 
 function App() {
   return (
-    <Login/>
+    <Home/>
   );
 }
 
--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -5,13 +5,36 @@ export default function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebarWrapper">
-                <img src="/assets/person/2.jpg" alt="" className="profilImg"/>
+                <img src="/assets/person/1.jpg" alt="" className="profilImg"/>
                 <div className="nameUser">
                     Name name
                 </div>
                 <ArrowDropDownIcon className="ArrowDropDownIcon" />
             </div>
             <hr className="sidebarHr"/>
+            <div className="friendsRequest">
+                    Friend request
+                    <ul className="sidebarListRequests">
+                        <li className="sidebarRequest">
+                            <img className="requestFriendImg" src="/assets/person/2.jpg" alt=""/>
+                            <span className="requestFriendName">Name name</span>
+                            <div className="buttons" >
+                                <button className="confirmButton">Confirm</button>
+                                <button className="deleteButton">Delete</button>
+                            </div>
+                        </li>
+                    </ul>
+            </div>
+            <hr className="sidebarHr"/>
+            <div className="friends">
+                    Friends
+                    <ul className="sidebarListFriends">
+                        <li className="sidebarFriends">
+                            <img className="friendImg" src="/assets/person/3.jpg" alt=""/>
+                            <span className="friendName">Name name</span>
+                        </li>
+                    </ul>
+            </div>
         </div>
     )
 }
