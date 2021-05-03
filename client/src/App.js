0080413e593ca066027a298a436fb852d808a9c2
Kaoutar Ahannuk <kaoutarahanuk@gmail.com>
2021-05-03 01:52:22 +0000

    DialogRegister

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+import RegistrationForm from './components/registrationForm/RegistrationForm';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 
 function App() {
   return (
-    <Login/>
+    <Login>
+      <RegistrationForm/>
+    </Login>
  
   );
 }
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,11 +1,56 @@
 import "./login.css";
-import { useState } from "react";
-import Popup from "../../components/Popup";
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
 
-export default function Login() {
-  const [openPopup, setOpenPopup] = useState(false)
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+export default function Login({children}) {
+  const [open, setOpen] = React.useState(false);
+
   const handleClickOpen = () => {
-    setOpenDialog(true);
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
   };
   return (
     <div className="login">
@@ -20,20 +65,21 @@ export default function Login() {
             <input placeholder="Password" className="loginInput" />
             <button className="loginButton">Sign in</button>
             <span className="loginForgot">Forgot Password?</span>
-            <button className="loginRegisterButton" onClick={
-              handleClickOpen
-            }>Sign Up</button>
+            <Button className="loginRegisterButton" onClick={handleClickOpen} >Sign Up</Button>
+            <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+              <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+                Sign Up
+              </DialogTitle>
+              <DialogContent dividers>
+                {children}
+              </DialogContent>
+            </Dialog>
           </form>
         </div>
         <div id="b" className="loginRight">
           <img className="loginImage" src="/assets/image_share.png"/>
         </div>
       </div>
-      <Popup
-       title="Registration Form"
-       openPopup={openPopup}
-       setOpenPopup={setOpenPopup}>
-      </Popup>
     </div>
     
   );
